7f4307ab75dda65887ed383c0b3ab4026ed04296
Akib <akibur_rahman@hotmail.com>
2018-11-18 17:04:09 +0600

    Code first Database Initialized

---
+++ HungryPanda/HungryPanda/Models/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HungryPanda.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+
+
+    }
+}
